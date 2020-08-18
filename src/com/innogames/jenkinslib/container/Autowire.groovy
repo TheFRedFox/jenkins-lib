5ea37c62bf6538e14f54a01b0e7c5ac1ad917ffa
Frederik Bauer <frederik.bauer@innogames.com>
2020-08-18 20:32:49 +0200

    Add Autowire annotation for services

---
+++ src/com/innogames/jenkinslib/container/Autowire.groovy
@@ -0,0 +1,9 @@
+package com.innogames.jenkinslib.container
+
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+
+@Retention(RetentionPolicy.RUNTIME)
+@interface Autowire {
+
+}
