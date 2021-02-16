34dc2b705a8087b9191c0f0b104945ebc0c347c0
Frederik Bauer <frederik.bauer@innogames.com>
2021-02-16 22:38:50 +0100

    container

--- vars/container.groovy
+++ vars/container.groovy
@@ -1,10 +1,10 @@
 import com.innogames.jenkinslib.container.Container
 import org.jenkinsci.plugins.workflow.cps.CpsScript
 
-def call(CpsScript script = null) {
+def call(CpsScript script = null, Map<String, Object> config = [:]) {
 	def container = Container.getInstance()
 	if (container == null) {
-		container = Container.newInstance(script)
+		container = Container.newInstance(script, config)
 	}
 
 	return container
