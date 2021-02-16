7856469e5ecae91a7f171ee8195eaf9088adeeee
Frederik Bauer <frederik.bauer@innogames.com>
2021-02-16 23:04:21 +0100

    Add logging for steps

---
+++ vars/log.groovy
@@ -0,0 +1,32 @@
+import com.innogames.jenkinslib.container.Container
+import com.innogames.jenkinslib.logger.LogLevel
+
+def call(Object message, String level) {
+	def container = Container.getInstance()
+	def logger = container.getLogger()
+	logger.log(message, LogLevel.valueOf(level))
+}
+
+def trace(Object message) {
+	call(message, 'TRACE')
+}
+
+def debug(Object message) {
+	call(message, 'DEBUG')
+}
+
+def info(Object message) {
+	call(message, 'INFO')
+}
+
+def warn(Object message) {
+	call(message, 'WARN')
+}
+
+def error(Object message) {
+	call(message, 'ERROR')
+}
+
+def critical(Object message) {
+	call(message, 'CRITICAL')
+}
