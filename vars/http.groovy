163d8bc89f5fbc6929d5940bbd9d1ff1923599de
Frederik Bauer <frederik.bauer@innogames.com>
2021-02-21 12:39:33 +0100

    http

--- vars/http.groovy
+++ vars/http.groovy
@@ -2,22 +2,28 @@
 import com.innogames.jenkinslib.container.Container
 import com.innogames.jenkinslib.http.HttpService
 
-def String get(String url) {
+static def call(String method, String url) {
+	def httpService = Container.getInstance().getComponent(HttpService)
+	return httpService.request(method, url)
+}
+
+
+static String get(String url) {
 	def httpService = Container.getInstance().getComponent(HttpService)
 	return httpService.httpGet(url)
 }
 
-def String post(String url, String payload) {
+static String post(String url, String payload) {
 	def httpService = Container.getInstance().getComponent(HttpService)
 	return httpService.httpPost(url, payload)
 }
 
-def String put(String url, String payload) {
+static String put(String url, String payload) {
 	def httpService = Container.getInstance().getComponent(HttpService)
 	return httpService.httpPut(url, payload)
 }
 
-def String delete(String url) {
+static String delete(String url) {
 	def httpService = Container.getInstance().getComponent(HttpService)
 	return httpService.httpDelete(url)
 }
