e34eacf9ac5f58b085c61f783d07e5e4f412fc37
Frederik Bauer <frederik.bauer@innogames.com>
2021-06-09 15:34:14 +0200

    test log

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,36 +1,36 @@
 import com.innogames.jenkinslib.container.Container
 import com.innogames.jenkinslib.logger.LogLevel
 
-def static call(Object message, LogLevel level = null) {
+def call(Object message, LogLevel level = null) {
 	def container = Container.getInstance()
 	def logger = container.getLogger()
 	logger.log(message, level)
 }
 
-def static call(Object message, String level) {
+def call(Object message, String level) {
 	call(message, LogLevel.valueOf(level))
 }
 
-def static trace(Object message) {
+def trace(Object message) {
 	call(message, LogLevel.TRACE)
 }
 
-def static debug(Object message) {
+def debug(Object message) {
 	call(message, LogLevel.DEBUG)
 }
 
-def static info(Object message) {
+def info(Object message) {
 	call(message, LogLevel.INFO)
 }
 
-def static warn(Object message) {
+def warn(Object message) {
 	call(message, LogLevel.WARN)
 }
 
-def static error(Object message) {
+def error(Object message) {
 	call(message, LogLevel.ERROR)
 }
 
-def static critical(Object message) {
+def critical(Object message) {
 	call(message, LogLevel.CRITICAL)
 }
